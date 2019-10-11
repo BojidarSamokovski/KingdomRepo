6b552408d768901040ab1327ac0c4687083c2f8f
BojidarSamokovski <bojidar_bgd@abv.bg>
2019-10-11 15:08:34 +0300

    KingdomDemo - update with project setup.

---
+++ KingdomDemo/KingdomDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KingdomDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
